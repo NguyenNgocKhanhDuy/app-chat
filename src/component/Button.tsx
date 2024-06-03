5c0885010d309311594f3748b7d9a5cc72dafe40
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2024-06-03 11:03:24 +0700

    validate

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,4 +1,4 @@
 import '../css/button.css'
 export default function Button(props : any) {
-    return (<button>{props.text}</button>)
+    return (<button onClick={props.onClick}>{props.text}</button>)
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,27 @@
 import Button from "../component/Button";
 import {Link} from "react-router-dom";
 import '../css/login.css'
+import {Simulate} from "react-dom/test-utils";
 
 export default function Login() {
+    const handleValidate = () => {
+        const inputUserName = document.querySelector("#username") as HTMLInputElement;
+        const inputPass = document.querySelector("#password") as HTMLInputElement;
+        const error = document.querySelector(".error") as HTMLDivElement;
+        const username = inputUserName.value;
+        const pass = inputPass.value;
+
+        if (username.length == 0 || pass.length == 0){
+            error.style.display = "flex";
+        }
+        console.log("1")
+    }
+
+    const handleHideError = () => {
+        const error = document.querySelector(".error") as HTMLDivElement;
+        error.style.display = "none"
+    }
+
     return (
       <div className={"container"}>
           <div className="box">
@@ -13,21 +32,25 @@ export default function Login() {
                       <label>Username</label>
                       <div className="holder">
                           <i className="fa-solid fa-user"></i>
-                          <input type="text" placeholder={"Username"}/>
+                          <input onChange={handleHideError} type="text" placeholder={"Username"} id={"username"}/>
                       </div>
                   </div>
                   <div className="wrapper">
                       <label>Password</label>
                       <div className="holder">
                           <i className="fa-solid fa-lock"></i>
-                          <input type="password" placeholder={"Password"}/>
+                          <input onChange={handleHideError} type="password" placeholder={"Password"} id={"password"}/>
                       </div>
                   </div>
+              </div>
+              <div className="error">
+                  <i className="fa-solid fa-circle-info"></i>
+                  <p className={"error-text"}>Please enter login information.</p>
               </div>
               <div className="forgot-pass">
                   <Link to={"forget-pass"}>Forgot your password?</Link>
               </div>
-              <Button text={"Login"}/>
+              <Button onClick={handleValidate} text={"Login"}/>
               <div className="register">
                   <p>
                       Not register yet?
