9db53b5cfb85fd294b07ac91c811ff7f4346bb05
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2024-06-21 21:26:07 +0700

    feat(Chat): chat to one person

--- src/component/ChatContent.tsx
+++ src/component/ChatContent.tsx
@@ -6,44 +6,45 @@ import WebSocketService from "../webSocket/webSocketService";
 
 export default function ChatContent(props : any) {
     const chatListRef = useRef<HTMLDivElement>(null);
-    // const user = props.user.user;
+    const user = props.user.user;
 
     useEffect(() => {
-        // WebSocketService.connect("ws://140.238.54.136:8080/chat/chat")
-        // WebSocketService.registerCallback('GET_PEOPLE_CHAT_MES', (data : any) => {
-        //     console.log(data)
-        //
-        //     var htmlItem = ``
-        //
-        //     for (let i = 0; i < data.length; i++) {
-        //         if (data[i].to != user){
-        //             console.log(user)
-        //             htmlItem += `<div class="item my-chat">
-        //                             <div class="text">
-        //                                 ${data[i].mes}
-        //                                 <span class="time">${data[i].createAt}</span>
-        //                             </div>
-        //                         </div>`
-        //         }else {
-        //             htmlItem += `<div class="item">
-        //                             <div class="user">
-        //                                 <img src=${avatar} class="avatar"/>
-        //                                 <p class="name">${data[i].name}</p>
-        //                             </div>
-        //                             <div class="text">
-        //                                 ${data[i].mes}
-        //                                 <span class="time">${data[i].createAt}</span>
-        //                             </div>
-        //                         </div>`
-        //         }
-        //     }
-        //
-        //     if (chatListRef.current) {
-        //         chatListRef.current.innerHTML = htmlItem;
-        //     }
-        // })
-        //
-        // WebSocketService.close()
+        WebSocketService.registerCallback('GET_PEOPLE_CHAT_MES', (data : any) => {
+            console.log(data)
+
+            var htmlItem = ``
+
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].to != user){
+                    console.log(user)
+                    htmlItem += `<div class="item my-chat">
+                                    <div class="text">
+                                        ${data[i].mes}
+                                        <span class="time">${data[i].createAt}</span>
+                                    </div>
+                                </div>`
+                }else {
+                    htmlItem += `<div class="item">
+                                    <div class="user">
+                                        <img src=${avatar} class="avatar"/>
+                                        <p class="name">${data[i].name}</p>
+                                    </div>
+                                    <div class="text">
+                                        ${data[i].mes}
+                                        <span class="time">${data[i].createAt}</span>
+                                    </div>
+                                </div>`
+                }
+            }
+
+            if (chatListRef.current) {
+                chatListRef.current.innerHTML = htmlItem;
+            }
+        })
+
+        WebSocketService.registerCallback('SEND_CHAT', (data: any) => {
+            handleGetChat()
+        })
 
 
         if (chatListRef.current) {
@@ -51,22 +52,63 @@ export default function ChatContent(props : any) {
         }
     }, []);
 
-    // const handleGetChat= () => {
-    //     WebSocketService.sendMessage(
-    //         {
-    //             action: 'onchat',
-    //             data: {
-    //                 event: 'GET_PEOPLE_CHAT_MES',
-    //                 data: {
-    //                     name: 'duy035',
-    //                     page:1
-    //                 }
-    //             }
-    //         }
-    //     )
-    // }
-    //
-    // handleGetChat()
+    const handleGetChat = () => {
+        var host = ""
+        if (user == "21130035") {
+            host = "duy035"
+            console.log("host")
+        }else {
+           host = "21130035"
+        }
+
+        WebSocketService.sendMessage(
+            {
+                action: 'onchat',
+                data: {
+                    event: 'GET_PEOPLE_CHAT_MES',
+                    data: {
+                        name: host,
+                        page:1
+                    }
+                }
+            }
+        )
+
+    }
+
+    handleGetChat()
+
+    const inputMessRef = useRef<HTMLInputElement>(null);
+    const handleSendChat = () => {
+        var userChatTo = ""
+        if (user == "21130035") {
+            userChatTo = "duy035"
+        }else {
+            userChatTo = "21130035"
+        }
+        var mess = ""
+        if (inputMessRef.current) {
+            mess = inputMessRef.current.value
+            console.log("log: "+mess)
+            WebSocketService.sendMessage(
+                {
+                    action: "onchat",
+                    data: {
+                        event: "SEND_CHAT",
+                        data: {
+                            type: "people",
+                            to: userChatTo,
+                            mes: mess
+                        }
+                    }
+                }
+            )
+            inputMessRef.current.value = ""
+            handleGetChat()
+        }else{
+            console.log("Input null")
+        }
+    }
 
     return (
         <div className="wrapper">
@@ -87,66 +129,66 @@ export default function ChatContent(props : any) {
                     <i className="fa-solid fa-ellipsis-vertical"></i>
                 </div>
             </div>
-            {/*<div className="content" ref={chatListRef}>*/}
-
-            {/*</div>*/}
-
             <div className="content" ref={chatListRef}>
-                <div className="item">
-                    <div className="user">
-                        <img src={avatar} className={"avatar"}/>
-                        <p className="name">Monica22</p>
-                    </div>
-                    <div className="text">
-                        Hi, how's going?
-                        <span className={"time"}>2024-06-21 11:40:12</span>
-                    </div>
-                </div>
-                <div className="item">
-                    <div className="user">
-                        <img src={avatar} className={"avatar"}/>
-                        <p className="name">Monica22</p>
-                    </div>
-                    <div className="text">
-                        Hi, how's going?
-                    </div>
-                </div>
-                <div className="item my-chat">
-                    <div className="text">
-                        Yes, you can swap with paying you network power.
-                        <span className={"time"}>2024-06-21 11:40:12</span>
-                    </div>
-                </div>
-                <div className="item my-chat">
-                    <div className="text">
-                        Hi, how's going?
-                    </div>
-                </div>
-                <div className="item">
-                    <div className="user">
-                        <img src={avatar} className={"avatar"}/>
-                        <p className="name">Monica22</p>
-                    </div>
-                    <div className="text">
-                        Hi, how's going?
-                    </div>
-                </div>
-                <div className="item">
-                    <div className="user">
-                        <img src={avatar} className={"avatar"}/>
-                        <p className="name">Monica22</p>
-                    </div>
-                    <div className="text">
-                        Hi, how's going?
-                    </div>
-                </div>
+
             </div>
+
+            {/*<div className="content" ref={chatListRef}>*/}
+            {/*    <div className="item">*/}
+            {/*        <div className="user">*/}
+            {/*            <img src={avatar} className={"avatar"}/>*/}
+            {/*            <p className="name">Monica22</p>*/}
+            {/*        </div>*/}
+            {/*        <div className="text">*/}
+            {/*            Hi, how's going?*/}
+            {/*            <span className={"time"}>2024-06-21 11:40:12</span>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div className="item">*/}
+            {/*        <div className="user">*/}
+            {/*            <img src={avatar} className={"avatar"}/>*/}
+            {/*            <p className="name">Monica22</p>*/}
+            {/*        </div>*/}
+            {/*        <div className="text">*/}
+            {/*            Hi, how's going?*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div className="item my-chat">*/}
+            {/*        <div className="text">*/}
+            {/*            Yes, you can swap with paying you network power.*/}
+            {/*            <span className={"time"}>2024-06-21 11:40:12</span>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div className="item my-chat">*/}
+            {/*        <div className="text">*/}
+            {/*            Hi, how's going?*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div className="item">*/}
+            {/*        <div className="user">*/}
+            {/*            <img src={avatar} className={"avatar"}/>*/}
+            {/*            <p className="name">Monica22</p>*/}
+            {/*        </div>*/}
+            {/*        <div className="text">*/}
+            {/*            Hi, how's going?*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div className="item">*/}
+            {/*        <div className="user">*/}
+            {/*            <img src={avatar} className={"avatar"}/>*/}
+            {/*            <p className="name">Monica22</p>*/}
+            {/*        </div>*/}
+            {/*        <div className="text">*/}
+            {/*            Hi, how's going?*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
             <div className="chat-action">
                 <div className="holder">
-                    <input type="text" placeholder={"Type here"}/>
+                    <input type="text" placeholder={"Type here"} ref={inputMessRef}/>
                     <i className="fa-solid fa-paperclip"></i>
                 </div>
-                <Button text={"Send"} className={"send"}/>
+                <Button text={"Send"} className={"send"} onClick={handleSendChat}/>
             </div>
         </div>
     );
--- src/webSocket/webSocketService.js
+++ src/webSocket/webSocketService.js
@@ -10,6 +10,7 @@ const WebSocketService = (() => {
         };
 
         socket.onmessage = (event) => {
+            console.log(event)
             const data = JSON.parse(event.data);
             (data.data && data.event == 'LOGIN') ? callbacks[data.event](data.data.RE_LOGIN_CODE || data.mes) : callbacks[data.event](data.data || data.mes)
         };
