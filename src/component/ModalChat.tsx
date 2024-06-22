4a0339e5c0a5eecafa61e2de84ec5018e93bc28e
SonHoangTran <hoangson145juzk@gmail.com>
2024-06-22 16:34:33 +0700

    Xy ly chuc nang send newchat

--- src/component/ModalChat.tsx
+++ src/component/ModalChat.tsx
@@ -1,7 +1,49 @@
 import Button from "./Button";
 import '../assets/css/modalRoom.scss';
+import {useEffect, useRef} from "react";
+import WebSocketService from "../webSocket/webSocketService";
+import avatar from "../assets/img/avatar.png";
 
 export default function ModalChat(props : any) {
+    const  inputMessRef = useRef<HTMLTextAreaElement>(null);
+    const  inputNameRef = useRef<HTMLInputElement>(null);
+
+
+    useEffect(() => {
+
+        WebSocketService.registerCallback('SEND_CHAT', (data: any) => {
+
+        })
+
+    }, []);
+    const handleSendChat = () => {
+        const name = inputNameRef.current ? inputNameRef.current.value : "";
+        var mess = ""
+        if (inputMessRef.current) {
+            mess = inputMessRef.current.value
+            console.log("log: " + mess)
+            WebSocketService.sendMessage(
+                {
+                    action: "onchat",
+                    data: {
+                        event: "SEND_CHAT",
+                        data: {
+                            type: "people",
+                            to: name,
+                            mes: mess
+                        }
+                    }
+                }
+            )
+        }
+            // inputMessRef.current.value = ""
+        // handleGetChat()
+        else {
+            console.log("Input null")
+        }
+        handleCloseModal();
+    }
+
     const handleCloseModal = () => {
         props.onClose();
     }
@@ -11,12 +53,9 @@ export default function ModalChat(props : any) {
             <div className="modal-container">
                 <i className="fa-solid fa-xmark close" onClick={handleCloseModal}></i>
                 <h2 className={"title"}>{props.modalText}</h2>
-                <input type="text" placeholder={"Input username"}/>
-                {/*<textarea type="text_message" placeholder={"Type your message here..."}/>*/}
-
-                <textarea className={"text_message"} placeholder="Type your message here..."></textarea>
-
-                <Button className="btn" text={props.btnText}/>
+                <input ref={inputNameRef} type="text" placeholder={"Input username"}/>
+                <textarea ref={inputMessRef} className={"text_message"} placeholder="Type your message here..."></textarea>
+                <Button className="btn" text={props.btnText} onClick={handleSendChat}/>
             </div>
         </div>
     )
